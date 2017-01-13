59af64cb302acdd623d872de80193e053ba97469
adrianperezmartin <adrianperezmartin@gmail.com>
2017-01-13 21:32:43 +0100

    IMC ult.version
    13/01/2017 a falta de crear otra actividad

--- CalcularIMC/app/src/main/java/com/example/adrian/calcularimc/Datos.java
+++ CalcularIMC/app/src/main/java/com/example/adrian/calcularimc/Datos.java
@@ -6,31 +6,32 @@ package com.example.adrian.calcularimc;
 
 public class Datos {
 
-    public String calculaIndice(int peso,int altura){
+    public String calculaIndice(float peso,float altura){
         //como pedimos la altura en cms y el calculo es en m2 lo convertimos para poder realizar las operaciones
-        double alturaCalculo = (altura*altura)/1000;
+        altura = (altura/100);
         //realizacomos el calculo y lo guardamos en una variable
-        double porcentaje = peso / alturaCalculo;
+        double porcentaje = peso / (altura*altura);
         //variable donde guardamos el resultado
-        String resultado= null;
+        String resultado;
 
-        if(porcentaje<=16){
+        if(porcentaje < 16){
             //desnutrido
+            resultado = "El porcentaje de Indice de masa corporal es: "+porcentaje+" Resultado: Desnutrido";
         }else if(porcentaje>16 && porcentaje <=18){
             //bajo de
-            resultado += "El porcentaje de Indice de masa corporal es: "+porcentaje+" Resultado: Bajo de peso";
+            resultado = "El porcentaje de Indice de masa corporal es: "+porcentaje+" Resultado: Bajo de peso";
         }else if(porcentaje>18 && porcentaje <=25){
             //normal
-            resultado += "El porcentaje de Indice de masa corporal es: "+porcentaje+" Resultado: Normal";
+            resultado = "El porcentaje de Indice de masa corporal es: "+porcentaje+" Resultado: Normal";
         }else if(porcentaje>25 && porcentaje <31){
             //sobrepeso
-            resultado += "El porcentaje de Indice de masa corporal es: "+porcentaje+" Resultado: Sobrepeso";
+            resultado = "El porcentaje de Indice de masa corporal es: "+porcentaje+" Resultado: Sobrepeso";
         }else if(porcentaje>=31){
             //obesidad
-            resultado += "El porcentaje de Indice de masa corporal es: "+porcentaje+" Resultado: Obesidad";
+            resultado = "El porcentaje de Indice de masa corporal es: "+porcentaje+" Resultado: Obesidad";
         }else{
             //No se puede mostrar su estado por favor vuelva a intentarlo
-            resultado += "No se ha podido calcular correctamente, por favor vuelva a intentarlo.";
+            resultado = "No se ha podido calcular correctamente, por favor vuelva a intentarlo.";
         }
         return resultado;
 
